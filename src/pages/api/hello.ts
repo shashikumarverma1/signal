a321c8aa48d02d75c062a826ea7835f95d029f85
shashi verma <shashikumarverma1996@gmail.com>
2025-03-03 17:03:54 +0530

    api route setup

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,14 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
-
-type Data = {
-  name: string;
-};
-
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>,
-) {
-  res.status(200).json({ name: "John Doe" });
+// pages/api/price.js
+export default function handler(req:any, res:any) {
+  if (req.method === 'POST') {
+      const { price, sl } = req.body;
+      // Process the received data (e.g., store in a database)
+      res.status(200).json({ message: 'Data received successfully.',
+      price: price,
+      sl: sl 
+       });
+  } else {
+      res.setHeader('Allow', ['POST']);
+      res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
 }
